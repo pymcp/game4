258315e8b93e6f527b8f2faeaab46b5dde7c6c94
Mike Patterson <mike.c.patterson@gmail.com>
2026-04-26 21:44:32 -0600

    data: CaravanSaveData resource

---
+++ scripts/data/caravan_save_data.gd
@@ -0,0 +1,12 @@
+## CaravanSaveData
+##
+## Snapshot of one player's caravan state for save/load.
+## Stored inside SaveGame.caravans (array of two, one per player).
+class_name CaravanSaveData
+extends Resource
+
+@export var player_id: int = 0
+## IDs of recruited party members.
+@export var recruited_ids: Array[StringName] = []
+## Serialized caravan inventory (from CaravanData.inventory.to_dict()).
+@export var inventory_data: Dictionary = {}
